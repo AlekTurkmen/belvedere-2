fcf5c42c41a89ea39b642a8477ae73402dae674a
Alek <alek11221@gmail.com>
2024-11-15 13:53:16 -0500

    added penguin logo

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -6,8 +6,14 @@ export default function Footer() {
   return (
     <footer className="bg-zinc-900 text-white px-6 py-6">
       <div className="flex flex-wrap justify-between items-center max-w-6xl mx-auto">
-        <Link href="/" className="hover:text-zinc-200 transition">
-          Home
+        <Link href="/" className="hover:text-zinc-200 transition flex items-center gap-2">
+          Belvedere
+          <img 
+            className="w-6 h-6 brightness-0 invert" 
+            src="https://www.svgrepo.com/show/322951/penguin.svg" 
+            loading="lazy" 
+            alt="belvedere penguin"
+          />
         </Link>
         <Link href="https://help.belvedere.alekturkmen.com/project-belvedere" className="hover:text-zinc-200 transition">
           Privacy Policy
--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -12,18 +12,15 @@ export default function Navlinks({ user }: NavlinksProps) {
     <div className="text-white px-6 py-6">
       <div className="flex flex-wrap justify-between items-center max-w-6xl mx-auto">
         <div className="flex-1 flex items-center">
-          <Link href="/" className="hover:text-zinc-200 transition">
-            Belvedere
-          </Link>
-          <div className="relative -right-2 -top-1">
-            {/* <Image
-              src="/public/BelvederePenguin.png"
-              alt=""
-              width={16}
-              height={16}
-              className="inline-block"
-            /> */}
-          </div>
+        <Link href="/" className="hover:text-zinc-200 transition flex items-center gap-2">
+          Belvedere
+          <img 
+            className="w-6 h-6 brightness-0 invert" 
+            src="https://www.svgrepo.com/show/322951/penguin.svg" 
+            loading="lazy" 
+            alt="belvedere penguin"
+          />
+        </Link>
         </div>
         <div className="flex justify-end flex-1">
           <Link href="/login" className="hover:text-zinc-200 transition">
